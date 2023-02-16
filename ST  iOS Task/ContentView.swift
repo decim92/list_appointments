ecaedd6fa451e8c1fe3dcb2e440d24a9baee38bc
decim92 <dgls@hotmail.es>
2023-02-16 15:42:20 -0500

    Initial Commit

---
+++ ST  iOS Task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ST  iOS Task
+//
+//  Created by Douglas Poveda on 16/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ST  iOS Task/ST__iOS_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ST__iOS_TaskApp.swift
+//  ST  iOS Task
+//
+//  Created by Douglas Poveda on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ST__iOS_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
